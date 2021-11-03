6cab94f962b1d362f189c092f7c753a84ffb656c
Joel <joellee@gic.com.sg>
2021-11-03 06:59:41 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,8 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "joelllee/s3-bucket/aws"
+  version = "2.2.0"
 
-  bucket = "my-s3-bucket2"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
 
